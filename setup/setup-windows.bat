3dfdaa2a0d61ea968c1d9cea1becccdd1fdf8522
Mikolka <mikolajchajewski@gmail.com>
2024-08-15 10:49:45 +0200

    new build tasks

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -10,10 +10,11 @@ haxelib install flixel-addons
 haxelib install flixel-ui
 haxelib install flixel-tools
 haxelib install SScript
-haxelib install hxCodec
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git
 haxelib install tjson
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git FlxPartialSound https://github.com/FunkinCrew/FlxPartialSound.git f986332ba5ab02abd386ce662578baf04904604a
 echo Finished!
 pause
