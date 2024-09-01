22dd7ba39a67708b81419fa424ec20b8f85fa860
Mikolka9144 <51335952+mikolka9144@users.noreply.github.com>
2024-09-01 19:56:18 +0200

    another change in deps

--- setup/windows.bat
+++ setup/windows.bat
@@ -11,7 +11,7 @@ haxelib install flixel-addons 3.0.2
 haxelib install flixel-ui 2.5.0
 haxelib install flixel-tools 1.5.1
 haxelib install tjson 1.4.0
-haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git fcfca29
+haxelib git hxcpp https://github.com/mikolka9144/pslice-hxcpp.git
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git 0a51aed0d9523d22a83e453ce7b593ec7fed4742
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
