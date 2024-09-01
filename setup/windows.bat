af95918114760c657e0be0988c7c3991fe3ea2dd
Mikolka9144 <51335952+mikolka9144@users.noreply.github.com>
2024-09-01 20:11:40 +0200

    fix deps

--- setup/windows.bat
+++ setup/windows.bat
@@ -15,7 +15,7 @@ haxelib git hxcpp https://github.com/mikolka9144/pslice-hxcpp.git
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git 0a51aed0d9523d22a83e453ce7b593ec7fed4742
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb07b04208cd014220207f8173acdb21
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc f9353b9edce10f4605d125dd1bda24ac36898bfb
 haxelib git FlxPartialSound https://github.com/FunkinCrew/FlxPartialSound.git f986332ba5ab02abd386ce662578baf04904604a
 echo Finished!
 pause
