d5130e4983eae86fa1272d732da6ba0db2432fee
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2024-09-23 22:00:44 +0200

    added main stage

--- assets/base_game/shared/stages/mainStageErect.lua
+++ assets/base_game/shared/stages/mainStageErect.lua
@@ -8,32 +8,40 @@
 
 function onCreate()
 	-- background shit
-	makeLuaSprite('stageback', 'stageback', -600, -300);
-	setScrollFactor('stageback', 0.9, 0.9);
-	
-	makeLuaSprite('stagefront', 'stagefront', -650, 600);
-	setScrollFactor('stagefront', 0.9, 0.9);
-	scaleObject('stagefront', 1.1, 1.1);
+	makeLuaSprite('stageback', 'erect/backDark', 729, -170);
 
+	makeAnimatedLuaSprite('stageCrowd', 'erect/crowd', 560, 290);
+	addAnimationByPrefix("stageCrowd", "idle", "Symbol 2 instance 1",12);
+	setScrollFactor('stageCrowd', 0.8, 0.8);
+
+	
+	makeLuaSprite('stagefront', 'erect/bg', -603, -187);
+
+	makeLuaSprite('server', 'erect/server', -361, 205);
+	
+	makeLuaSprite('lights', 'erect/lights', -601, -147);
+	setScrollFactor('lights', 1.2, 1.2);
 	-- sprites that only load if Low Quality is turned off
 	if not lowQuality then
-		makeLuaSprite('stagelight_left', 'stage_light', -125, -100);
-		setScrollFactor('stagelight_left', 0.9, 0.9);
-		scaleObject('stagelight_left', 1.1, 1.1);
+		makeLuaSprite('stagelight_small', 'erect/brightLightSmall', 967, -103);
+		setScrollFactor('stagelight_small', 1.2, 1.2);
 		
-		makeLuaSprite('stagelight_right', 'stage_light', 1225, -100);
-		setScrollFactor('stagelight_right', 0.9, 0.9);
-		scaleObject('stagelight_right', 1.1, 1.1);
-		setProperty('stagelight_right.flipX', true); --mirror sprite horizontally
+		makeLuaSprite('orangeLOL', 'erect/orangeLight', 189, -195);
 
-		makeLuaSprite('stagecurtains', 'stagecurtains', -500, -300);
-		setScrollFactor('stagecurtains', 1.3, 1.3);
-		scaleObject('stagecurtains', 0.9, 0.9);
+		makeLuaSprite('greenLOL', 'erect/lightgreen', -171, 242);
+		makeLuaSprite('redLOL', 'erect/lightred', -101, 560);
+		makeLuaSprite('TheOneAbove', 'erect/lightAbove', 804, -117);
 	end
 
 	addLuaSprite('stageback', false);
+	addLuaSprite('stageCrowd', false);
+	addLuaSprite('stagelight_small', false);
 	addLuaSprite('stagefront', false);
-	addLuaSprite('stagelight_left', false);
-	addLuaSprite('stagelight_right', false);
-	addLuaSprite('stagecurtains', false);
+	addLuaSprite('server', false);
+	addLuaSprite('greenLOL', false);
+	addLuaSprite('redLOL', false);
+	addLuaSprite('orangeLOL', false);
+
+	addLuaSprite('lights', false);
+	addLuaSprite('TheOneAbove', false);
 end
