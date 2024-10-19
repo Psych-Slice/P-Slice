56881fffe8fe8875fb868e5e0d75cc98feca89c1
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2024-10-19 22:49:30 +0200

    fix shadow's work

--- source/objects/NoteSplash.hx
+++ source/objects/NoteSplash.hx
@@ -62,7 +62,7 @@ class NoteSplash extends FlxSprite
 
 		var skin:String = splash;
 		if (skin == null || skin.length < 1)
-			skin = try PlayState.SONG.splashSkin catch(e) null;
+			skin =  PlayState.SONG?.splashSkin;
 
 		if (skin == null || skin.length < 1)
 			skin = DEFAULT_SKIN + getSplashSkinPostfix();
