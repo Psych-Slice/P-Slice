4aa1aabb369f7898bb3b2f55d246ff25d3ca5d2b
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2024-10-19 22:01:51 +0200

    fix version issue

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -93,7 +93,7 @@ class TitleState extends MusicBeatState
 			http.onData = function(data:String)
 			{
 				updateVersion = data.split('\n')[0].trim();
-				var curVersion:String = MainMenuState.psychEngineVersion.trim();
+				var curVersion:String = MainMenuState.pSliceVersion.trim();
 				trace('version online: ' + updateVersion + ', your version: ' + curVersion);
 				if (updateVersion != curVersion)
 				{
