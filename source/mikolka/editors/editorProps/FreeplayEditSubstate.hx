4cc2520447c0b37365fee6322cc65e34333bdbe4
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2024-12-02 17:03:52 +0100

    added new controld

--- source/mikolka/editors/editorProps/FreeplayEditSubstate.hx
+++ source/mikolka/editors/editorProps/FreeplayEditSubstate.hx
@@ -4,7 +4,6 @@ import mikolka.funkin.freeplay.FreeplayStyle;
 import mikolka.funkin.freeplay.FreeplayStyleRegistry;
 import shaders.AngleMask;
 import mikolka.vslice.freeplay.backcards.BoyfriendCard;
-import mikolka.vslice.freeplay.DJBoyfriend.FreeplayDJ;
 
 class FreeplayEditSubstate extends MusicBeatSubstate {
     
@@ -28,6 +27,23 @@ class FreeplayEditSubstate extends MusicBeatSubstate {
 	var input_text1:PsychUIInputText;
 	var input_text2:PsychUIInputText;
 	var input_text3:PsychUIInputText;
+    //DJ EDITOR
+	var steper_introStartFrame:PsychUINumericStepper;
+	var steper_introEndFrame:PsychUINumericStepper;
+	var steper_loopStartFrame:PsychUINumericStepper;
+	var steper_loopEndFrame:PsychUINumericStepper;
+	var steper_introBadStartFrame:PsychUINumericStepper;
+	var steper_loopBadEndFrame:PsychUINumericStepper;
+	var steper_loopBadStartFrame:PsychUINumericStepper;
+	var steper_introBadEndFrame:PsychUINumericStepper;
+    //ANIMATION
+	var list_animations:PsychUIDropDownMenu;
+	var input_animName:PsychUIInputText;
+	var input_animPrefix:PsychUIInputText;
+	var btn_newAnim:PsychUIButton;
+	var btn_trashAnim:PsychUIButton;
+	var stepper_offset_x:PsychUINumericStepper;
+	var stepper_offset_y:PsychUINumericStepper;
 
     public function new(player:PlayableCharacter) {
         super();
@@ -86,6 +102,7 @@ class FreeplayEditSubstate extends MusicBeatSubstate {
     }
     override function update(elapsed:Float) {
         super.update(elapsed);
+        if(!loaded) return;
         if(PsychUIInputText.focusOn == null)
             {
                 ClientPrefs.toggleVolumeKeys(true);
@@ -102,7 +119,7 @@ class FreeplayEditSubstate extends MusicBeatSubstate {
             }
         else ClientPrefs.toggleVolumeKeys(false);
 		
-		if(dj_anim.activeSprite != null && loaded){
+		if(dj_anim.activeSprite != null){
 			if(controls.UI_DOWN_P) dj_anim.selectAnim(1);
 			if(controls.UI_UP_P) dj_anim.selectAnim(-1);
 			if(FlxG.keys.justPressed.SPACE) dj_anim.playAnim();
@@ -120,9 +137,9 @@ class FreeplayEditSubstate extends MusicBeatSubstate {
     
             // GENERAL
             @:privateAccess
-            input_assetPath = new PsychUIInputText(10, 20, 100, data._data.freeplayDJ.assetPath);
+            input_assetPath = new PsychUIInputText(10, 20, 150, data._data.freeplayDJ.assetPath);
     
-            btn_reload = new PsychUIButton(130, 20, "Reload", () ->
+            btn_reload = new PsychUIButton(180, 20, "Reload", () ->
             {
                 remove(dj);
                 dj.destroy();
@@ -133,34 +150,42 @@ class FreeplayEditSubstate extends MusicBeatSubstate {
             
             @:privateAccess{
                 input_text1 = new PsychUIInputText(10,50,150,data._data.freeplayDJ.text1);
-                input_text2 = new PsychUIInputText(10,90,150,data._data.freeplayDJ.text2);
-                input_text3 = new PsychUIInputText(10,130,150,data._data.freeplayDJ.text3);
+                input_text2 = new PsychUIInputText(10,70,150,data._data.freeplayDJ.text2);
+                input_text3 = new PsychUIInputText(10,90,150,data._data.freeplayDJ.text3);
             }
-            //BF
-            //GF
+            //DJ EDITOR
+            var dj_editor_desc_txt = new FlxText(10,10,400,"Pick frames (start,end)");
+            dj_editor_desc_txt.setFormat(Paths.font("vcr.ttf"),20,FlxColor.WHITE,LEFT,OUTLINE_FAST,FlxColor.BLACK);
+            
+            var txt_introStart = new FlxText(10,50,0,"Victory intro:",10);
+            steper_introStartFrame = new PsychUINumericStepper(100,50,1,data.getFreeplayDJData().getFistPumpIntroStartFrame(),0,100);
+            steper_introEndFrame = new PsychUINumericStepper(170,50,1,data.getFreeplayDJData().getFistPumpIntroEndFrame(),-1,100);
 
-            // var btn_gf_prev:PsychUIButton = new PsychUIButton(20, 20, "Anims preview", ()->
-            //     {
-            //         animPreview.attachSprite(gfChill); 
-            //         PsychUIInputText.focusOn = null;
-            //     });
-            // var btn_gf_reload:PsychUIButton = new PsychUIButton(120, 20, "Reload", ()->
-            //     {
-            //         switchEditorGF(activePlayer._data.charSelect.gf);
-            //     });
-            // input_gfAssetPath = new PsychUIInputText(20, 60, 100, activePlayer._data.charSelect.gf.assetPath);
-            // input_gfAssetPath.onChange = (p,next) -> {
-            //     activePlayer._data.charSelect.gf.assetPath = next;
-            // };
-            // input_gfAnimInfoPath = new PsychUIInputText(20, 120, 100, activePlayer._data.charSelect.gf.animInfoPath);
-            // input_gfAnimInfoPath.onChange = (prev,next) ->{
-            //     activePlayer._data.charSelect.gf.animInfoPath = next;
-            // };
-            // chkBox_visualiser = new PsychUICheckBox(20,150,"Use visualiser",100,() -> {
-            //     activePlayer._data.charSelect.gf.visualizer = chkBox_visualiser.checked;
-            // });
-            // chkBox_visualiser.checked = activePlayer._data.charSelect.gf.visualizer;
+            var txt_introLoop = new FlxText(10,90,0,"Victory loop:",10);
+            steper_loopStartFrame = new PsychUINumericStepper(100,90,1,data.getFreeplayDJData().getFistPumpLoopStartFrame(),0,100);
+            steper_loopEndFrame = new PsychUINumericStepper(170,90,1,data.getFreeplayDJData().getFistPumpLoopEndFrame(),-1,100);
 
+            var txt_introBadStart = new FlxText(10,130,0,"Loss intro:",10);
+            steper_introBadStartFrame = new PsychUINumericStepper(100,130,1,data.getFreeplayDJData().getFistPumpIntroBadStartFrame(),0,100);
+            steper_introBadEndFrame = new PsychUINumericStepper(170,130,1,data.getFreeplayDJData().getFistPumpIntroBadEndFrame(),-1,100);
+
+            var txt_introBadLoop = new FlxText(10,170,0,"Loss loop:",10);
+            steper_loopBadStartFrame = new PsychUINumericStepper(100,170,1,data.getFreeplayDJData().getFistPumpLoopBadStartFrame(),0,100);
+            steper_loopBadEndFrame = new PsychUINumericStepper(170,170,1,data.getFreeplayDJData().getFistPumpLoopBadEndFrame(),-1,100);
+            //Animation
+            list_animations = new PsychUIDropDownMenu(10,10,["lol"],(index,name) ->{
+                trace(name);
+            });
+            btn_newAnim = new PsychUIButton(140,10,"New",() -> {
+
+            },50);
+            btn_trashAnim = new PsychUIButton(200,10,"Delete",() -> {
+
+            },50);
+            input_animName = new PsychUIInputText(10,50,150,"Name");
+            input_animPrefix = new PsychUIInputText(10,90,150,"Prefix");
+            stepper_offset_x = new PsychUINumericStepper(20,130,1,0);
+            stepper_offset_y = new PsychUINumericStepper(85,130,1,0);
             //?
     
             //GENERAL
@@ -171,26 +196,41 @@ class FreeplayEditSubstate extends MusicBeatSubstate {
             tab.add(input_assetPath);
             tab.add(btn_reload);
     
-            tab.add(newLabel(input_text1, "Scroll text:"));
+            tab.add(newLabel(input_text1, "Scroll texts:"));
             tab.add(input_text1);
             tab.add(input_text2);
             tab.add(input_text3);
     
-            //BF
-    
+            //DJ EDITOR
             var tab = UI_box.getTab("DJ Editor").menu;
+            tab.add(dj_editor_desc_txt);
+            tab.add(txt_introStart);
+            tab.add(steper_introStartFrame);
+            tab.add(steper_introStartFrame);
+            tab.add(txt_introLoop);
+            tab.add(steper_loopStartFrame);
+            tab.add(steper_loopEndFrame);
+            tab.add(txt_introBadStart);
+            tab.add(steper_introBadStartFrame);
+            tab.add(steper_introBadEndFrame);
+            tab.add(txt_introBadLoop);
+            tab.add(steper_loopBadStartFrame);
+            tab.add(steper_loopBadEndFrame);
+            tab.add(steper_introEndFrame);
             //tab.add(btn_player_prev);
-    
+            
             //GF
             var tab = UI_box.getTab("Animation").menu;
-            // tab.add(btn_gf_prev);
-            // tab.add(btn_gf_reload);
-            // tab.add(newLabel(input_gfAssetPath, "Asset path:"));
-            // tab.add(input_gfAssetPath);
-            // tab.add(newLabel(input_gfAnimInfoPath, "JSFL anim folder:"));
-            // tab.add(input_gfAnimInfoPath);
-            // tab.add(chkBox_visualiser);
-            //
+            tab.add(btn_newAnim);
+            tab.add(btn_trashAnim);
+            tab.add(newLabel(input_animName,"Name"));
+            tab.add(input_animName);
+            tab.add(newLabel(input_animPrefix,"Prefix"));
+            tab.add(input_animPrefix);
+            tab.add(new FlxText(10, 110, 100, "Offsets (x,y)"));
+            tab.add(stepper_offset_x);
+            tab.add(stepper_offset_y);
+            tab.add(list_animations);
         }
     function newLabel(ref:FlxSprite, text:String)
     {
