f0100f6138873662a57a1718a8890fd1422dc0b7
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2024-12-21 12:44:45 +0100

    added softcoded 'new' tag

--- source/mikolka/compatibility/FreeplaySongData.hx
+++ source/mikolka/compatibility/FreeplaySongData.hx
@@ -59,6 +59,9 @@ using mikolka.funkin.utils.ArrayTools;
  
          var meta = FreeplayMeta.getMeta(songId);
          difficultyRating = meta.songRating;
+            
+
+         isNew = meta.allowNewTag;
          freeplayPrevStart = meta.freeplayPrevStart/meta.freeplaySongLength;
          freeplayPrevEnd = meta.freeplayPrevEnd/meta.freeplaySongLength;
          albumId = meta.albumId;
@@ -137,7 +140,15 @@ using mikolka.funkin.utils.ArrayTools;
          // this.difficultyRating = songDifficulty.difficultyRating;
          this.scoringRank = Scoring.calculateRankForSong(Highscore.formatSong(songId, loadAndGetDiffId()));
  
-         this.isNew = false; // song.isSongNew(currentDifficulty);
+         var wasCompleted = false;
+         var saveSongName = Paths.formatToSongPath(songId);
+         for (x in Highscore.songScores.keys()){
+            if(x.startsWith(saveSongName) && Highscore.songScores[x] > 0){
+                wasCompleted = true;
+                break;
+            }
+         }
+         isNew = (( ClientPrefs.data.vsliceForceNewTag || isNew) && !wasCompleted); 
      }
      public function loadAndGetDiffId() {
          var leWeek:WeekData = WeekData.weeksLoaded.get(WeekData.weeksList[levelId]);
