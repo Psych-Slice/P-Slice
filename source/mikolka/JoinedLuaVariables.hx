556b3e8cac99d72868d1c5f57b664274691b09ae
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2024-12-25 11:56:54 +0100

    Revert JoinedLuaVariables.hx

--- source/mikolka/JoinedLuaVariables.hx
+++ source/mikolka/JoinedLuaVariables.hx
@@ -9,19 +9,8 @@ class JoinedLuaVariables extends StringMap<Dynamic> {
     }
     override function get(key:String):Null<Dynamic> {
         //! P-Slice patch
-        var regex = ~/(.+)\[([0-9]+)\]/;
-        if(regex.match(key)){
-            var realKey = regex.matched(1);
-            var index = Std.parseInt(regex.matched(2));
-            var arrayValue = super.get(realKey);
-            if(arrayValue == null) arrayValue = resolveUnknown(realKey);
-            return arrayValue[index];
-        }
         var arrayValue = super.get(key);
-        if(arrayValue == null) arrayValue = resolveUnknown(key);
+        if(arrayValue == null) arrayValue = LuaUtils.getVarInArray(PlayState.instance, key, false);
         return arrayValue;
-    }
-    private inline function resolveUnknown(key:String):Null<Dynamic> {
-        return LuaUtils.getVarInArray(PlayState.instance, key, false);
     }
 }
