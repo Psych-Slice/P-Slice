edf35e3d6722411886fb62f7d499321741d5c04c
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-01-13 23:52:44 +0100

    add some translations

--- source/options/BaseGameSubState.hx
+++ source/options/BaseGameSubState.hx
@@ -2,33 +2,33 @@ package options;
 
 class BaseGameSubState extends BaseOptionsMenu {
     public function new() {
-        title = "V-Slice settings";
+        title = Language.getPhrase("vslice_menu","V-Slice settings");
         rpcTitle = "V-Slice settings menu";
         var option:Option = new Option('Freeplay dynamic coloring',
 			'Enables dynamic freeplay background color. Disable this if you prefer original V-slice freeplay menu colors',
 			'vsliceFreeplayColors',
-			'bool');
+			BOOL);
 		addOption(option);
 
 		var option:Option = new Option('Use results screen',
 			'If disabled will skip showing the result screen',
 			'vsliceResults',
-			'bool');
+			BOOL);
 		addOption(option);
 		var option:Option = new Option('Smooth health bar',
 			'If enabled makes health bar move more smoothly',
 			'vsliceSmoothBar',
-			'bool');
+			BOOL,);
 		addOption(option);
 		var option:Option = new Option('Special freeplay cards',
 			'If disabled will force every character to use BF\'s card (including pico)',
 			'vsliceSpecialCards',
-			'bool');
+			BOOL);
 		addOption(option);
 		var option:Option = new Option('Force "New" tag',
 			'If enabled will force every uncompleted song to show "new" tag even if it\'s disabled',
 			'vsliceForceNewTag',
-			'bool');
+			BOOL);
 		addOption(option);
         super();
     }
