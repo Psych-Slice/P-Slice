795ac9bad35ef290e9e28cdfcac282718b13bd2c
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-01-15 18:03:51 +0100

    fixed splashes

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -539,7 +539,9 @@ class NoteOffsetState extends MusicBeatState
 
 		var str:String;
 		var str2:String;
-		if(onComboMenu){
+		final accept:String = (controls.mobileC) ? "A" : (!controls.controllerMode) ? "ACCEPT" : "Start";
+		if(onComboMenu)
+		{
 			str = Language.getPhrase('combo_offset', 'Combo Offset');
 			#if TOUCH_CONTROLS_ALLOWED
 			addTouchPad('NONE', 'A_B_C');
@@ -553,12 +555,7 @@ class NoteOffsetState extends MusicBeatState
 			#end
 		}
 
-		if(controls.mobileC)
-			str2 = '(Press A to Switch)';
-		else if(!controls.controllerMode)
-			str2 = Language.getPhrase('switch_on_accept', '(Press Accept to Switch)');
-		else
-			str2 = Language.getPhrase('switch_on_start', '(Press Start to Switch)');
+		str2 = Language.getPhrase('switch_on_button', '(Press {1} to Switch)', [accept]);
 
 		changeModeText.text = '< ${str.toUpperCase()} ${str2.toUpperCase()} >';
 	}
