5c9003340b03e19e419d59aeda723e6b2cc48f53
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-02-18 22:04:26 +0100

    update setup scripts

--- setup/mobile.sh
+++ setup/mobile.sh
@@ -12,7 +12,7 @@ haxelib install flixel-addons 3.2.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
 haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
-haxelib git flxanimate https://github.com/Psych-Slice/FlxAnimate.git 18091dfeb629ba2805a5f3e10f5de80433080359 --quiet
+haxelib git flxanimate https://github.com/Psych-Slice/FlxAnimate.git 42f1b5d193b4345ca7d6933380ab3105985b44a3 --quiet
 haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit --quiet
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc f9353b9edce10f4605d125dd1bda24ac36898bfb --quiet --skip-dependencies
 haxelib git hxvlc https://github.com/MobilePorting/hxvlc --quiet --skip-dependencies
