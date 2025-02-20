dee98f7bbb7b4721d335d107d0456c53b3653d46
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-02-20 23:50:58 +0100

    Update credits

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -82,11 +82,11 @@ class CreditsState extends MusicBeatState
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
 			['P-Slice Engine Team'],
-			['Mikolka9144',			'mikolka',			'Did everything for P-slice',								 	'https://gamebanana.com/members/3329541',	'2ebcfa'],
+			['Mikolka9144',			'mikolka',			'The lead for the mod',								 'https://gamebanana.com/members/3329541',									'2ebcfa'],
 			[""],
 			['P-Slice Contributors'],
 			["Fazecarl",			'fazecarl',			'Made the new logo for P-Slice',									'https://gamebanana.com/members/2121406',	'29170a'],
-			["Mykarm",			'mykarm',			'Made the new icon for P-Slice',									'https://x.com/cronviersmeat/status/1849059676467417311?s=46&t=4dcTT7PAMkRJ8zYd4LgTow',	'29170a'],
+			["Mykarm",				'mykarm',			'Made the new icon for P-Slice',									'https://x.com/cronviersmeat/status/1849059676467417311?s=46&t=4dcTT7PAMkRJ8zYd4LgTow',	'29170a'],
 			[""],
 			["P-Slice server"],
 			["Join our community",	"ppslice",			"Hi!",																"https://discord.gg/9FCyCqEvRf",			"5e36c4"],
