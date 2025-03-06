53dae0fd17a077341ab7f94e6a5bd5da26a1d716
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-03-06 11:17:58 +0100

    minor fixes

--- source/states/HTML5LoadingScreen.hx
+++ source/states/HTML5LoadingScreen.hx
@@ -659,7 +659,6 @@ class HTML5LoadingScreen extends MusicBeatState { // JS exclusive loading screen
     
                 var isAnimateAtlas:Bool = false;
                 var img:String = character.image;
-                trace(img);
                 img = img.trim();
                 #if flxanimate
                 var animToFind:String = Paths.getPath('images/$img/Animation.json', TEXT);
