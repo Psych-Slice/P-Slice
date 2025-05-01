d864274c9d8d1ad85633f349a8cc2d079f181876
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-05-01 22:17:15 +0200

    there was a missing hash?

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,7 +4,7 @@ cd ..
 @echo on 
 echo Installing dependencies.
 haxelib install hscript 2.5.0
-haxelib install lime 8.0.1
+haxelib install lime 8.0.1 
 haxelib install openfl 9.2.1
 haxelib install flixel 5.2.2
 haxelib install flixel-addons 3.0.2
@@ -14,7 +14,7 @@ haxelib install tjson 1.4.0
 haxelib git hxcpp https://github.com/mikolka9144/pslice-hxcpp.git
 haxelib git hxCodec https://github.com/polybiusproxy/hxCodec.git 0a51aed0d9523d22a83e453ce7b593ec7fed4742
 haxelib git flxanimate https://github.com/Psych-Slice/FlxAnimate.git 18091dfeb629ba2805a5f3e10f5de80433080359
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc f9353b9edce10f4605d125dd1bda24ac36898bfb
 haxelib git FlxPartialSound https://github.com/FunkinCrew/FlxPartialSound.git f986332ba5ab02abd386ce662578baf04904604a
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
