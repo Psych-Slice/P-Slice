c86fa5bb99a7f793fc0e0083798d541829ecb7c1
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-05-14 12:08:03 +0200

    update mobile compile deps

--- setup/mobile.sh
+++ setup/mobile.sh
@@ -3,8 +3,8 @@ echo Making the haxelib and setuping folder in same time...
 #haxelib newrepo
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib git hxcpp https://github.com/MobilePorting/hxcpp  --quiet
-haxelib git lime https://github.com/MobilePorting/lime --quiet 608f3f7196b2b58df19afff4aedec5bdda107301
+haxelib git hxcpp https://github.com/Psych-Slice/hxcpp-mobile.git  --quiet
+haxelib git lime https://github.com/Psych-Slice/lime-mobile.git --quiet 608f3f7196b2b58df19afff4aedec5bdda107301
 haxelib install flixel 6.0.0 --quiet --skip-dependencies
 haxelib install openfl  9.4.1 --quiet --skip-dependencies
 haxelib install flixel-addons 3.3.2 --quiet --skip-dependencies
@@ -12,9 +12,9 @@ haxelib install flixel-tools 1.5.1 --quiet --skip-dependencies
 haxelib install hscript-iris 1.1.3 --quiet 
 haxelib install tjson 1.4.0 --quiet 
 haxelib git flxanimate https://github.com/Psych-Slice/FlxAnimate.git 42f1b5d193b4345ca7d6933380ab3105985b44a3 --quiet
-haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit --quiet
+haxelib git linc_luajit https://github.com/Psych-Slice/linc_luajit-mobile.git --quiet
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc f9353b9edce10f4605d125dd1bda24ac36898bfb --quiet --skip-dependencies
-haxelib git hxvlc https://github.com/MobilePorting/hxvlc --quiet --skip-dependencies
+haxelib git hxvlc https://github.com/Psych-Slice/hxvlc-mobile.git --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 haxelib git FlxPartialSound https://github.com/FunkinDroidTeam/FlxPartialSound.git 2b7943ba50eb41cf8f70e1f2089a5bd7ef242947 --quiet
