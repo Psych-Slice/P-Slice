f2c6da06a9a09bb8b40e7ce2bba7e850fe31c0e0
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-06-12 01:49:13 +0200

    Revert unnecessary changes

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -681,12 +681,6 @@ class PlayState extends MusicBeatState
 		cachePopUpScore();
 
 		if(eventNotes.length < 1) checkEventNote();
-
-		for (i in 0...unspawnNotes.length) {
-        	if (unspawnNotes[i].isSustainNote) {
-            	    unspawnNotes[i].noAnimation = true;
-		    }
-		}
 	}
 
 	function set_songSpeed(value:Float):Float
@@ -3318,12 +3312,6 @@ class PlayState extends MusicBeatState
 		spawnHoldSplashOnNote(note);
 
 		if (!note.isSustainNote) invalidateNote(note);
-
-		if (note.gfNote || note.noteType == 'GF Sing') {
-            	    gf.holdTimer = 0;
-        	} else {
-            	    dad.holdTimer = 0;
-		}
 	}
 
 	public function goodNoteHit(note:Note):Void
@@ -3432,12 +3420,6 @@ class PlayState extends MusicBeatState
 		if(result != LuaUtils.Function_Stop && result != LuaUtils.Function_StopHScript && result != LuaUtils.Function_StopAll) callOnHScript('goodNoteHit', [note]);
 		spawnHoldSplashOnNote(note);
 		if(!note.isSustainNote) invalidateNote(note);
-
-		if (note.gfNote || note.noteType == 'GF Sing') {
-            	    gf.holdTimer = 0;
-        	} else {
-           	    boyfriend.holdTimer = 0;
-		}
 	}
 
 	public function invalidateNote(note:Note):Void {
