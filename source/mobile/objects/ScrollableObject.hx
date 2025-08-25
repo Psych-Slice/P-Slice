1728970edb3ee73fedc612e2421fd92b62c08a5c
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-08-26 01:49:24 +0200

    i fone fixes. Please test the song preview thing. It doesn't work at all in the simulator for some reason

--- source/mobile/objects/ScrollableObject.hx
+++ source/mobile/objects/ScrollableObject.hx
@@ -39,11 +39,13 @@ class ScrollableObject extends TouchZone {
         }
         else if( #if mobile TouchUtil.justReleased #else FlxG.mouse.justReleased #end) {
             if(isTapping){
-                #if mobile
-                if(getjustPressed()?.overlaps(clickButton,clickButton.camera) ?? false) onTap.dispatch();
-                #else
-                if(FlxG.mouse.overlaps(clickButton,clickButton.camera)) onTap.dispatch();
-                #end
+                if(clickButton.camera == null || clickButton.camera.scroll != null){
+                    #if mobile
+                    if(getjustPressed()?.overlaps(clickButton,clickButton.camera) ?? false) onTap.dispatch();
+                    #else
+                    if(FlxG.mouse.overlaps(clickButton,clickButton.camera)) onTap.dispatch();
+                    #end
+                }
                 isTapping = false;
             }
             else if(isDragging) {
