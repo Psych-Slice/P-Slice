a10de3496968c2666c2849dfc96bc0cbe5c58993
cigsCICKgars <cigsfridayfunkin@gmail.com>
2025-08-27 18:16:42 -0400

    Better Restart effect + Ghost Note effect on Shit and Bad scores
    Makes it so the restart effect (minus changing difficulty) will now instead do something more accurate to the base game, i also made it so when you get shitty note ratings the notes desaturate and become more ghost like, not 100% accurate but it looks good

--- source/mikolka/compatibility/funkin/FunkinCamera.hx
+++ source/mikolka/compatibility/funkin/FunkinCamera.hx
@@ -2,9 +2,8 @@ package mikolka.compatibility.funkin;
 import backend.PsychCamera;
 
 class FunkinCamera extends PsychCamera {
-    var camName:String;
-    public function new(name:String,X:Float = 0, Y:Float = 0, Width:Int = 0, Height:Int = 0, Zoom:Float = 0) {
-        camName = name;
-        super(X,Y,Width,Height,Zoom);
+    //removed name as its super now has this, just kinda made more sense, sorry
+    public function new(name:String, X:Int = 0, Y:Int = 0, Width:Int = 0, Height:Int = 0, Zoom:Float = 1) {
+        super(name, X, Y, Width, Height, Zoom);
     }
 }
