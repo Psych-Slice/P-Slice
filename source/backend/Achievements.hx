eb3a589980ed5664760e32b9b83a90324aacc143
Axolotlguy13212 <practicleant1321@outlook.com>
2025-09-20 13:30:45 -0500

    Started adding the thing where you get an achievement for getting 69% (like in V-Slice)

--- source/backend/Achievements.hx
+++ source/backend/Achievements.hx
@@ -47,6 +47,7 @@ class Achievements {
 		#end
 		createAchievement('ur_bad',					{name: "L", description: "Complete a Song with a rating lower than 20%."});
 		createAchievement('ur_good',				{name: "You Should Drink More Water", description: "Complete a Song with a rating of 100%."});
+		createAchievement('ur_nice',				{name: "Nice", description: "Complete a Song with a rating of 69%."});
 		createAchievement('oversinging', 			{name: "Oversinging Much...?", description: "Sing for 10 seconds without going back to Idle."});
 		createAchievement('hype',					{name: "Hyperactive", description: "Finish a Song without going back to Idle."});
 		createAchievement('two_keys',				{name: "Just the Two of Us", description: "Finish a Song pressing only two keys."});
