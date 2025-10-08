0b46b08f3c90c3f7491433aca13c901d77d00340
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-10-08 23:48:24 +0200

    update screen util

--- source/mobile/backend/ScreenUtil.hx
+++ source/mobile/backend/ScreenUtil.hx
@@ -78,22 +78,22 @@ class ScreenUtil
     // see: https://developer.apple.com/documentation/uikit/uiview/safeareainsets
     switch (displayOrientation)
     {
-      case LANDSCAPE: // landscape
+      case DISPLAY_ORIENTATION_LANDSCAPE: // landscape
         notchRect.width = leftInset + rightInset;
         notchRect.height = bottomInset - topInset;
         notchRect.y = topInset;
-      case LANDSCAPE_FLIPPED: // landscape
+      case DISPLAY_ORIENTATION_LANDSCAPE_FLIPPED: // landscape
         notchRect.width = leftInset + rightInset;
         notchRect.height = bottomInset - topInset;
         notchRect.y = topInset;
         notchRect.x = deviceWidth - notchRect.width; // move notchRect if we are flipped, notch is at the right of screen
-      case PORTRAIT: // portrait
+      case DISPLAY_ORIENTATION_PORTRAIT: // portrait
         notchRect.width = deviceWidth;
         notchRect.height = topInset;
-      case PORTRAIT_FLIPPED: // portrait
+      case DISPLAY_ORIENTATION_PORTRAIT_FLIPPED: // portrait
         notchRect.width = deviceWidth;
         notchRect.height = bottomInset;
-        notchRect.y = deviceHeight - notchRect.height; // move notchRect if we are flipped, the notch is at the bottom of screen
+        notchRect.y = deviceHeight - notchRect.height; // move notchRect if we are flipped, the notch is at the bottom of
       default: // display orientation unknown? perhaps this occurs on desktop
     }
     #end
